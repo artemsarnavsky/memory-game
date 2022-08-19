3eec7e16d1efd0616cf18531882223c051d44a90
artemsarnavsky <110407305+artemsarnavsky@users.noreply.github.com>
2022-08-19 17:46:29 +0300

    game

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
+// масив карток
 const cards = [
-    {
-      name: "html",
-      img:
-        "https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
-    },
-    {
-      name: "html",
-      img:
-        "https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
-    },
-    {
-      name: "html",
-      img:
-        "https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
-    },
-    {
-      name: "html",
-      img:
-        "https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
-    },
-    {
-      name: "html",
-      img:
-        "https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
-    }
-  ];
+	{
+		name: "html",
+		img:
+			"https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true"
+	},
+	{
+		name: "css",
+		img: "https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true"
+	},
+	{
+		name: "JavaScript",
+		img: "https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true"
+	},
+	{
+		name: "python",
+		img:
+			"https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true"
+	},
+	{
+		name: "sass",
+		img: "https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true"
+	}
+];
 
- var choosenCards = [];
- var choosenCardsId = [];
+var  choosenCards = [];
+var  choosenCardsId = [];
 
 
 
-// перебираемо картки 
-  var playCards = document.querySelectorAll("div.box");
-  for (let i = 0; i < playCards.length; i++) {
-
-    playCards[i].setAttribute("id", i) // Встановлюемо ид для кожнои картки 
-    playCards[i].addEventListener("click", function (){
-    this.style.backgroundImage = "url(${+shuffleArray[i].img})";
-    
-
-
-      });
+var playCards = document.querySelectorAll("div.box");
+for (let i = 0; i < playCards.length; i++) {
+	playCards[i].setAttribute("id", i); // встановлюємо id для кожної картки
+	playCards[i].addEventListener("click", function () {
+	this.style.backgroundImage = `url(${shuffleArray[i].img})`;
+  choosenCards.push(shuffleArray[this.id].name)
+  choosenCardsId.push(this.id)
+  if (choosenCards.length === 2) {
+    console.log("check")
+     setTimeout(function(){
+      if (choosenCards[0] === choosenCards[1]){
+      playCards[choosenCardsId[0]].style.visibility = "hidden"
+      playCards[choosenCardsId[1]].style.visibility = "hidden"
   }
-  let array = [...cards, ...cards],
-    shuffleArray = array.sort(() => 0.5 - Math.random());
+
+  else {
+     playCards[choosenCardsId[0]].style.backgroundImage = "none"
+     playCards[choosenCardsId[1]].style.backgroundImage = "none"
+
+  }
+  choosenCards = [];
+  choosenCardsId = [];
+
+
+     }, 500);
+
+  }
+
   
-  console.log(array[1]);
+
+
+
+
+	});
+}
+
+let array = [...cards, ...cards],
+	shuffleArray = array.sort(() => 0.5 - Math.random());
+
